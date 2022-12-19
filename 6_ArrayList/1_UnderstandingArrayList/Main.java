9a2098b784af95cb26817c282f870a7b7d6300e8
Yrol <yrollgayanth@yahoo.com>
2022-12-19 20:38:02 +1100

    Working with ArrayLists

---
+++ 6_ArrayList/1_UnderstandingArrayList/Main.java
@@ -0,0 +1,41 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    /*
+     * - ArrayList is an implementation of the List interface (java.util.List)
+     * - ArrayLists are similar to arrays and they're internally using an array.
+     * - The internal array is an array of objects.
+     * - ArrayList uses generics, hence we can specify the object type on creation.
+     * - Defining the Object type is optional when creating the ArrayList.
+     * - By default ArrayList is type Object (if the type is not defined inside <>).
+     * - Object type must be defined inside the diamond operator.
+     * - Initial capacity/size of the ArrayList can be define on creation.
+     */
+    public static void main(String args[]) {
+
+        // Defining an ArrayList (without an object type - this will be type Object by
+        // default)
+        ArrayList myList = new ArrayList<>();
+
+        // Defining ArrayList with type Object.
+        // Since every class in Java is type Object base, we can store Double, String &
+        // etc in this list.
+        ArrayList<Object> myList2 = new ArrayList<Object>();
+        // ArrayList<Object> myList2 = new ArrayList<>(); // Valid - can also define
+        // without type on right hand side
+
+        // ArrayList accepts only Strings
+        ArrayList<String> myStringList = new ArrayList<>();
+
+        // Using the List interface directly instead of implementation ArrayList
+        List<String> myList3 = new ArrayList<>();
+        // ArrayList<String> myList4 = new List<>(); // Not valid
+
+        // initial capacity/size of the ArrayList and will automatically expands.
+        List<String> myList4 = new ArrayList<>(20);
+
+    }
+}
---
+++ 6_ArrayList/2_ArrayListMethods/Main.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    /*
+     * Important methods:
+     * - add, remove and set
+     * - empty, size, clear and contains
+     * - equals - in ArrayList equals operation is a custom implementation where it
+     * checks the elements of two list to be exactly the same including index
+     * position.
+     */
+    public static void main(String args[]) {
+
+        /*
+         * Add
+         */
+
+        // Creating a generic list
+        List list = new ArrayList<>();
+
+        // Adding values
+        list.add("Dog");
+        list.add(5);
+
+        // Printing the list (will call the toString() method automatically)
+        // list.toString() is not necessary
+        System.out.println(list);// [Dog, 5]
+
+        // Creating a type String list and add elements
+        List<String> pets = new ArrayList();
+        pets.add("Cat");
+        // pets.add(4); // Invalid. Only Strings are allowed
+
+        // adding to a specific index (at 0)
+        pets.add(0, "Dog");
+        System.out.println(pets);// ["Dog", "Cat"]
+
+        pets.add(1, "Parrot");
+        System.out.println(pets);// ["Dog", "Parrot", "Cat"]
+
+        /*
+         * Remove
+         * - Can remove by value (first matching) or by index
+         */
+        List<String> companies = new ArrayList();
+        companies.add("Google");
+        companies.add("Microsoft");
+        companies.add("Toyota");
+        companies.add("Google");
+        System.out.println(companies); // [Google, Microsoft, Toyota, Google]
+        companies.remove("Google"); // remove first occurrence of Google
+        System.out.println(companies); // [Microsoft, Toyota, Google]
+        companies.remove(0); // [Toyota, Google]
+        System.out.println(companies);
+
+        /*
+         * Set
+         * - Setting value at a specific index (replace)
+         */
+        companies.set(0, "Microsoft");
+        System.out.println(companies); // [Microsoft, Google]
+
+        // tricky scenarios
+
+        // Attempting to set a value on an invalid index
+        // companies.set(6, "Google"); // will throw out of bound exception
+
+        /*
+         * Size and Empty
+         */
+
+        // isEmpty() (return boolean)
+        List<String> brands = new ArrayList();
+        System.out.println(brands.isEmpty()); // true (boolean)
+
+        // size() - return size of the list (in int)
+        System.out.println(brands.size());
+
+        // Another way to check if the list is empty
+        if (brands.size() == 0) {
+            System.out.println("No brands found");
+        }
+
+        brands.add("Pepsi");
+        System.out.println(brands.isEmpty()); // false
+        System.out.println(brands.size()); // size
+
+        /*
+         * Clear - remove all the list of the list
+         */
+
+        brands.clear(); // will remove all elements
+        System.out.println(brands.isEmpty()); // true
+        System.out.println(brands.size());// 0
+
+        /*
+         * Contains - returns a boolean
+         */
+        brands.add("McDonalds");
+        System.out.println(brands.contains("McDonalds")); // true
+        System.out.println(brands.contains("Coca-Cola")); // false
+
+        /*
+         * Equals
+         */
+        brands.add("KFC");
+        List<String> newBrands = new ArrayList();
+        newBrands.add("McDonalds");
+        newBrands.add("KFC");
+
+        System.out.println(newBrands.equals(brands)); // true
+
+        newBrands.add("IBM");
+        System.out.println(newBrands.equals(brands));// false
+
+        // same elements on different indexes
+        brands.add(0, "IBM");
+        System.out.println(brands);
+        System.out.println(newBrands);
+        System.out.println(newBrands.equals(brands)); // false
+    }
+}
