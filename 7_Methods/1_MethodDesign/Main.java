52a66dde66b3f7f23fa08ab8e05f31ef1325457b
Yrol <yrollgayanth@yahoo.com>
2022-12-26 21:45:05 +1100

    Method design modification

--- 7_Methods/1_MethodDesign/Main.java
+++ 7_Methods/1_MethodDesign/Main.java
@@ -25,6 +25,9 @@ public class Main {
     void jump3() {
     }
 
+    static final void jump10() {
+    }
+
     // Ex4: Optional specifiers - static and final
     public static final void jump4() {
     }
@@ -49,6 +52,40 @@ public class Main {
 
     // Won't compile as multiple types are not allowed to be returned.
     // public String int void jump10() {}
+
+    /*
+     * Method naming conventions
+     * - Similar to variables discussed in: 1_Basics/9_VariableScope/Main.java
+     * - Can start with $ or _
+     * - Cannot use Java keywords as method names
+     * - First character cannot be a number
+     * - By conventions methods begin with lowercase. however can use uppercase.
+     */
+
+    // Ex1: valid
+    public void test() {
+    }
+
+    // Ex2: Valid - star with $
+    public String $test() {
+        return "";
+    }
+
+    // Ex3: valid star with _
+    public int _getInt() {
+        return 22;
+    }
+
+    // Ex4: valid - starting with uppercase
+    public char MyChar(char ch) {
+        return ch;
+    }
+
+    // Ex2: Invalid
+    // public void 2test(){} // won't compile
+
+    // Ex3: Invalid as trying to use Java keywords
+    // public void public(){} won't compile
 
     public static void main(String args[]) {
 
