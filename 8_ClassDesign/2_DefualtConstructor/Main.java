77316ce83f2b716471324d7c8322c9d3d442f3f5
Yrol <yrollgayanth@yahoo.com>
2023-01-02 11:42:51 +1100

    Defualt constructor

---
+++ 8_ClassDesign/2_DefualtConstructor/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    /*
+     * By default if the constructor isn't specified, Java will generate constructor
+     * automatically know as the default constructor.
+     */
+    public static void main(String[] args) {
+
+        // Instantiate using the default constructor
+        Main main = new Main();
+    }
+}
